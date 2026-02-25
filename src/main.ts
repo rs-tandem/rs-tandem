1620e0f873498e35ad15fa0ec084c768c79fa6e2
oreopk <sart6zaV@yandex.ru>
2026-02-25 21:40:50 +0300

    fix: clear main

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,3 @@
 import './styles/main.css';
-import { Button, Input } from './shared/components';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hi app`;
-
-const app = document.querySelector<HTMLDivElement>('#app');
-
-if (app) {
-  const greyButton = new Button('Регистрация', 'grey');
-  const orangeButton = new Button('Войти!', 'orange');
-  const yellowButton = new Button('Начнем подтотовку!', 'yellow');
-  const yellowButton2 = new Button('Начнем подтотовку!', 'yellow');
-  const input = new Input('E-mail', 'text');
-  const input2 = new Input('E-mail', 'text');
-  app.append(
-    greyButton.getElement(),
-    orangeButton.getElement(),
-    yellowButton.getElement(),
-    input.getWrapperElement(),
-    yellowButton2.getElement(),
-    input2.getWrapperElement(),
-  );
-  input2.setError(true);
-  yellowButton2.setDisabled(true);
-}
