afa61b3dbbfabf693cd428de8ffc86f8c69ac7cb
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2026-03-02 17:19:34 -0800

    feat: add topics types and topicsService

---
+++ src/features/topics/TopicsService.ts
@@ -0,0 +1,24 @@
+import type { Topic } from './topics.types';
+
+export const TOPICS: Topic[] = [
+  {
+    id: 'core-js',
+    title: 'Основы JavaScript',
+    imageUrl: '/assets/img/js_logo.png',
+  },
+  {
+    id: 'closures',
+    title: 'Замыкания',
+    imageUrl: '/assets/img/closures.png',
+  },
+  {
+    id: 'asynchrony',
+    title: 'Асинхронность',
+    imageUrl: '/assets/img/async.png',
+  },
+  {
+    id: 'data-structures',
+    title: 'Структуры данных',
+    imageUrl: '/assets/img/tree.png',
+  },
+];
