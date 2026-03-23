0efa3106f58e3b40046eed00eea8662fc621c4a9
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2026-03-23 10:37:03 -0700

    fix: remove home and back buttons

--- src/features/error/NotFoundPage.ts
+++ src/features/error/NotFoundPage.ts
@@ -1,8 +1,5 @@
-import { Router } from 'vanilla-routing';
-
-import { Button } from '../../shared/components';
 import { DOMHelper } from '../../shared/utils/createElement';
-import { authService } from '../auth/AuthService';
+
 import './NotFoundPage.css';
 
 export class NotFoundPage {
@@ -25,20 +22,8 @@ export class NotFoundPage {
       'not-found__description',
       'Похоже, такой страницы не существует.',
     );
-    const actions = DOMHelper.createElement('div', 'not-found__actions');
-    const backButton = new Button('Назад', 'grey', () => {
-      Router.go(authService.isAuthenticated() ? '/' : '/auth');
-    });
 
-    const homeButton = new Button('На главную', 'grey', () => {
-      if (authService.isAuthenticated()) {
-        Router.go('/');
-      } else {
-        Router.go('/auth');
-      }
-    });
-
-    const rightImg = DOMHelper.createElement('div', 'right-img');
+    const rightImg = DOMHelper.createElement('div', 'not-found__right-img');
     const notFoundImg = DOMHelper.createElement('img', 'not-found__image');
     notFoundImg.src = new URL(
       '../../assets/img/mascot_sad_transparent.png',
@@ -46,8 +31,8 @@ export class NotFoundPage {
     ).toString();
 
     rightImg.append(notFoundImg);
-    actions.append(backButton.getElement(), homeButton.getElement());
-    container.append(title, description, actions);
+
+    container.append(title, description);
     this.element.append(container, rightImg);
   }
 
